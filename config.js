62345ea456c54c407faa8f1804c7006f29d692f5
Forever Living <arvin8786@gmail.com>
2026-04-15 17:46:14 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+// config.js - THE ONLY PLACE TO UPDATE YOUR URL
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbz2c20JQhD5v-bHsdQassiK6pA3ekHuWyTY63WeiNeCYCL9I0ZizvDus8ouJtNRV6ie/exec";
