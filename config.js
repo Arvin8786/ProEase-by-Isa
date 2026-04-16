bfaf2d9f44bfe4ed777a6760484b0ad9b137631a
Forever Living <arvin8786@gmail.com>
2026-04-16 11:25:00 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js - THE MASTER CONTROL CENTER
 
-// 1. Your Google Apps Script URL
+// 1. Your Google Apps Script URL (Update this whenever you deploy a New Version)
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwT6ULwITdv7RFAOkflgUYO_PRRGCR1uFhsyRc6wgw9WCkRv5rmm7YfXydQyZS7ZqtV/exec";
 
 // 2. Global App Settings
